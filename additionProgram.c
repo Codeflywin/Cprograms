875c1326dbfad35b48e700e9a66b3d06ead47454
codeflywin@gmail.com <codeflywin@gmail.com>
2026-08-26 11:17:51 -0500

    added additionProgram

---
+++ additionProgram.c
@@ -0,0 +1,29 @@
+//Call the library
+#include <stdio.h>
+
+int main() {
+//Create the values to be entered by the user, dividend will be a constant = 2
+    int val1;
+    int val2;
+    int sumtotal;
+//Prompt the user to enter the values
+
+    printf("Enter the first number: ");
+    scanf("%d", &val1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &val2);
+
+//Need the sum of the numbers between values 1 and 2
+if (val1 <= val2) {
+    for (int i = val1; i <= val2; i++) {
+        sumtotal += i;
+    }
+} else {
+    for (int i = val1; i >= val2; i--) {
+        sumtotal += i;
+    }
+}
+    printf("The sum of the numbers between %d and %d inclusive is: %d\n", val1, val2, sumtotal);
+    return 0;
+}
