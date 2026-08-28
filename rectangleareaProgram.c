7a831a12d7de25a129c6848155262662da513e66
codeflywin@gmail.com <codeflywin@gmail.com>
2026-08-27 19:36:21 -0500

    changes to rectangleareaProgram.c

--- rectangleareaProgram.c
+++ rectangleareaProgram.c
@@ -1,5 +1,10 @@
 //Call the library
 #include <stdio.h>
+//Calculate the area with a function outside of the main
+    int areaCalculation (int length, int width)
+    {
+        return length * width;
+    }
 
 int main() {
     //Create the values to be entered by the user
@@ -14,12 +19,13 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &width);
 
-//Calculate the area with a function
-    int area (int length, int width);
-    return length * width;
+//Call the function and calculate the
+    int area = areaCalculation(length, width);
 
 //Print the result
     printf("The area of the rectangle is %d\n", area);
+
+//End program
     return 0;
 
 }
